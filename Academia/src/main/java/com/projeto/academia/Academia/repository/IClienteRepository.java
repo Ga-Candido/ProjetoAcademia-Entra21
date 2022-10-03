d19201752db0cc5bea2f09c7d263915464e5f0af
Geiso-Herrera <104040316+Geiso-Herrera@users.noreply.github.com>
2022-10-03 20:02:00 -0300

    atualizado

--- Academia/src/main/java/com/projeto/academia/Academia/repository/IClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.academia.Academia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.academia.Academia.model.Cliente;
-
-public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
-
-}
--- Academia/src/main/java/com/projeto/academia/Academia/repository/IFuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.academia.Academia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.academia.Academia.model.Funcionario;
-
-public interface IFuncionarioRepository extends JpaRepository<Funcionario, Integer>{
-
-}
